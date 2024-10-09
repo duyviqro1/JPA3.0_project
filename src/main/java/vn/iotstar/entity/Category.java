a754b42a9c2431ec2345a512a15d61c66ce8ecb7
duyviqro1 <anhduy0969@gmail.com>
2024-10-09 08:50:23 +0700

    category

---
+++ src/main/java/vn/iotstar/entity/Category.java
@@ -0,0 +1,52 @@
+package vn.iotstar.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "categories")
+@NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c")
+
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "CategoryId")
+	private int categoryId;
+
+	@Column(name = "Categoryname", columnDefinition = "nvarchar(200) NOT NULL")
+	@NotEmpty(message = "Không được phép rỗng")
+	private String categoryname;
+
+	@Column(name = "Images", columnDefinition = "nvarchar(500) NULL")
+	private String images;
+
+	@Column(name = "Status")
+	private int status;
+
+	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+	private List<Video> videos;
+
+
+
+	public Video addVideo(Video video) {
+		getVideos().add(video);
+		video.setCategory(this);
+		
+		return video;
+		}
+	
+	public Video removeVideo(Video video) {
+		getVideos().remove(video);
+		video.setCategory(null);
+		
+		return video;
+		}
+}
---
+++ src/main/webapp/view/admin/category-add.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	<%@ taglib prefix ="c" uri = "jakarta.tags.core" %>
+<form action="${pageContext.request.contextPath}/admin/category/insert" method="post" enctype = "multipart/form-data">
+	<label for="Categoryname">Category Name:</label>
+	<br> 
+		<input type="text" id="Categoryname" name="Categoryname" >
+	<br> 
+		<label for="images">images:</label>
+	<br> 
+		<img height="150" width="200" src="" id="imagess"/>
+		<input type="file" onchange="chooseFile(this)" id="imagess" name="images">
+	<br>
+		<label for="status">status:</label>
+	<br> 
+		<input type="radio" id="ston" name="status" value="1" checked>
+	<label for="html">Hoạt động</label><br>
+	
+	<input type="radio" id="stoff" name="status" value="0">
+	<label for="css">Khóa</label><br>
+
+	<br> <input type="submit" value="Submit">
+</form>
